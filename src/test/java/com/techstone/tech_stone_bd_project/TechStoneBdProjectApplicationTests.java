e555f47d7f4501754344081f081b6e6e28396dd3
Amimul1234 <1805056@ugrad.cse.buet.ac.bd>
2021-11-06 15:27:43 +0600

    First commit

---
+++ src/test/java/com/techstone/tech_stone_bd_project/TechStoneBdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstone.tech_stone_bd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechStoneBdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
