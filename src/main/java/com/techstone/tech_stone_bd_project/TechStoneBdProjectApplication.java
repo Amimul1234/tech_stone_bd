5ca55450263c78ce6afc640ff798f95fed5793c3
Amimul1234 <1805056@ugrad.cse.buet.ac.bd>
2021-11-06 16:20:40 +0600

    Swagger configured for apis

--- src/main/java/com/techstone/tech_stone_bd_project/TechStoneBdProjectApplication.java
+++ src/main/java/com/techstone/tech_stone_bd_project/TechStoneBdProjectApplication.java
@@ -7,9 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class TechStoneBdProjectApplication {
-
     public static void main( String[] args ) {
         SpringApplication.run(TechStoneBdProjectApplication.class, args);
     }
-
 }
